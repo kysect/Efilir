46536225163d31b1067a023a8834c34763b1d600
Inredika WhiteBlack <inredikawb@gmail.com>
2018-02-09 02:14:40 +0300

    [v0.1.1] Added Food

---
+++ GenericLife/Models/IBaseCell.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Media;
+
+namespace GenericLife.Models
+{
+    public interface IBaseCell
+    {
+        int PositionX { get; set; }
+        int PositionY { get; set; }
+
+        Color GetColor();
+    }
+}
---
+++ GenericLife/Tools/TestingPolygon.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Documents;
+using GenericLife.Models;
+using GenericLife.Services;
+
+namespace GenericLife.Tools
+{
+    public class TestingPolygon
+    {
+        private readonly DrawingService _ds;
+        private CellFieldService _cellFieldService;
+
+        public TestingPolygon(DrawingService ds, CellFieldService cellFieldService)
+        {
+            _ds = ds;
+            _cellFieldService = cellFieldService;
+
+            Init();
+        }
+
+        private void Init()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                int x = GlobalRand.Next(100);
+                int y = GlobalRand.Next(100);
+                if (_cellFieldService.GetPointType(x, y) == PointType.Void)
+                {
+                    _cellFieldService.Cells.Add(new SimpleCell(_cellFieldService, x, y, 100));
+                }
+            }
+
+            for (int i = 0; i < 200; i++)
+            {
+                int x = GlobalRand.Next(100);
+                int y = GlobalRand.Next(100);
+                if (_cellFieldService.GetPointType(x, y) == PointType.Void)
+                {
+                    _cellFieldService.Foods.Add(new FoodCell(x, y));
+                }
+            }
+
+            UpdateUi();
+        }
+
+        public void UpdateUi()
+        {
+            _ds.ClearBlack();
+            _ds.DrawPoints(_cellFieldService.Cells);
+            _ds.DrawPoints(_cellFieldService.Foods);
+        }
+        public void RandomMove()
+        {
+            _cellFieldService.RandomMove();
+            UpdateUi();
+        }
+    }
+}
