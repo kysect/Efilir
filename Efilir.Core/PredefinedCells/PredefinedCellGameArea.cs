60f5874609698bdb4cfb7e39f3d907e8c6c09641
Fredi Kats <fredikats@outlook.com>
2021-06-01 15:09:52 +0300

    feat: optimize algorithm for calculation cell movements

---
+++ Efilir.Core/PredefinedCells/PredefinedCellGameArea.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Efilir.Core.Environment;
+using Efilir.Core.PredefinedCells.Cells;
+
+namespace Efilir.Core.PredefinedCells
+{
+    public class PredefinedCellGameArea : GameArea
+    {
+        public List<PredefinedCell> PredefinedCells { get; }
+
+        public PredefinedCellGameArea(int areaSize) : base(areaSize)
+        {
+            PredefinedCells = new List<PredefinedCell>();
+        }
+    }
+}
