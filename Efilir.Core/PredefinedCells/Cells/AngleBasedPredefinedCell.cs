d99a049605bad5bf70e48acaf1fe8a892e0530d9
Fredi Kats <fredikats@outlook.com>
2021-06-05 15:41:10 +0300

    feat: randomize turn angle

--- Efilir.Core/PredefinedCells/Cells/AngleBasedPredefinedCell.cs
+++ Efilir.Core/PredefinedCells/Cells/AngleBasedPredefinedCell.cs
@@ -23,31 +23,40 @@ namespace Efilir.Core.PredefinedCells.Cells
         {
             int weight = 0;
 
+            int stepNumber = 0;
             foreach (List<(PredefinedCellType, Vector)> stepOnIteration in GameArea.PreviousSteps)
-            foreach ((PredefinedCellType type, Vector predefinedCellPosition) in stepOnIteration)
             {
-                Vector moveDirection = predefinedCellPosition - RealPosition;
+                stepNumber++;
+                foreach ((PredefinedCellType type, Vector predefinedCellPosition) in stepOnIteration)
+                {
+                    Vector moveDirection = predefinedCellPosition - RealPosition;
 
-                if (moveDirection.Length() > Configuration.MaxLengthForInteraction || moveDirection.Length() < double.Epsilon)
-                    continue;
+                    if (moveDirection.Length() > Configuration.MaxLengthForInteraction || moveDirection.Length() < double.Epsilon)
+                        continue;
 
-                Angle angleToObject = VelocityDirection.AngleTo(moveDirection);
-                if (angleToObject > Configuration.CellVisibleAngle)
-                    continue;
+                    Angle angleToObject = VelocityDirection.AngleTo(moveDirection);
+                    if (angleToObject > Configuration.CellVisibleAngle)
+                        continue;
 
-                if (angleToObject > Angle.Zero)
-                    weight++;
+                    if (angleToObject > Angle.Zero)
+                        weight = 1 / stepNumber;
 
-                if (angleToObject < Angle.Zero)
-                    weight--;
+                    if (angleToObject < Angle.Zero)
+                        weight -= 1 / stepNumber;
+                }
             }
 
             if (weight > 0)
-                return Configuration.TurnAngleChange;
+                return GenerateRandomAngle();
             if (weight < 0)
-                return Configuration.TurnAngleChange * -1;
+                return GenerateRandomAngle() * -1;
 
             return Angle.FromDegree(0);
+        }
+
+        private Angle GenerateRandomAngle()
+        {
+            return Configuration.TurnAngleChange * ((GlobalRand.Next(100) + 1.0) / 100);
         }
     }
 }
